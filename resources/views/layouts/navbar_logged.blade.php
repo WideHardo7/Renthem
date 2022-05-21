789f7fc9c42192988f26a7b605ad29837fdc8d73
Elisa Pace <s1084405@studenti.univpm.it>
2022-05-21 15:28:13 +0200

    aggiunta menu dedicati per utenza

--- resources/views/layouts/navbar_logged.blade.php
+++ resources/views/layouts/navbar_logged.blade.php
@@ -1,7 +1,19 @@
-<?php
+!-- navbar Starts -->
+<div class="navbar-wrapper">
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
-
+        <div class="navbar-inverse" role="navigation">
+          <div class="container">                        
+            <!-- Nav Starts -->
+            <div class="navbar-collapse  collapse">
+              <ul class="nav navbar-nav navbar-right">
+               <li class="active"><a href="index.php">Home</a></li>
+                <li><a href="<?php echo "index.php"; ?>#aboutContatti">Contatti</a></li>
+                <li><a href="<?php echo "index.php"; ?>#aboutContatti">About Us</a></li>         
+                <li><a href="<?php echo "index.php"; ?>#faq">FAQ</a></li>
+                <li><a href="">Profilo</a></li>
+              </ul>
+            </div>
+            <!-- #Nav Ends -->
+          </div>
+        </div>
+    </div>
