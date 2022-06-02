0243f3a531c99afee7edb4b993e6cd235ab78795
WideHardo7 <marzilianoluca@yahoo.com>
2022-06-02 17:36:00 +0200

    peepoPoopoo but peepo is not pooping :/ and peepo is angry >:(

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -17,7 +17,7 @@
                     <h4>Dati Azienda</h4>
                     <p><b>Renthem Immobiliare Studenti</b><br>
                     
-                        <span class="glyphicon glyphicon-map-marker"></span><a href="https://www.google.it/maps/place/Presidenza+Ingegneria+-+UnivPM/@43.5869019,13.5120902,17z/data=!3m1!4b1!4m5!3m4!1s0x132d81a66b4d2c41:0xeef23e89a11e8451!8m2!3d43.586902!4d13.5165749"> via Brecce Bianche 12, Italia </a><br>
+<span class="glyphicon glyphicon-map-marker"></span><a href="https://www.google.it/maps/place/Presidenza+Ingegneria+-+UnivPM/@43.5869019,13.5120902,17z/data=!3m1!4b1!4m5!3m4!1s0x132d81a66b4d2c41:0xeef23e89a11e8451!8m2!3d43.586902!4d13.5165749"> via Brecce Bianche 12, Italia </a><br>
 <span class="glyphicon glyphicon-envelope"></span> renthem@affitti.com<br>
 <span class="glyphicon glyphicon-earphone"></span> 327 1234567</p> 
             </div>
